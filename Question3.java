c222ddc916804198a6d401495851dd6c834d90ff
AlbertWC <albertwc0718@gmail.com>
2020-02-18 22:36:00 +0800

    Question 3 Explanation

--- Question3.java
+++ Question3.java
@@ -5,14 +5,18 @@ public class Question3
     public static void main(String[] agrs)
     {
         Scanner sc = new Scanner(System.in);
+        //user input the string
         String x = sc.nextLine();
+        //convert the string into lowercase before do the checker
         x = x.toLowerCase();
         String temp = "";
+        //execute palindrome and store the reversed string
         for(int i = x.length(); i > 0 ;i--)
         {
             temp += x.charAt(i-1);
         }
         System.out.println(temp);
+        //check original string and reversed string is matched 
         if(temp.equals(x))
         {
             System.out.println("True");
