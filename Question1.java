c1761bb2481f722d1b2a86e44da8ee25f1c39b5c
AlbertWC <albertwc0718@gmail.com>
2020-02-18 22:17:59 +0800

    Questions 1 Explanation

--- Question1.java
+++ Question1.java
@@ -5,6 +5,7 @@ public class Question1
         int a = 1 ;
         while ( a < 100)
         {
+            //if the modulus of 3 or 5 getting 0 which means that the value is the factor of 3 or 5. 
             if(a % 3 == 0|| a % 5 == 0)
             {
                 System.out.println(a);
