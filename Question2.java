b85515f8a6e7abe886f83917361ed0f1a110d3d8
AlbertWC <albertwc0718@gmail.com>
2020-02-18 22:29:36 +0800

    Question 2 explanation

--- Question2.java
+++ Question2.java
@@ -8,7 +8,7 @@ public class Question2
         int[] x2 = {-3, -1, 0, 3, 21};
         int[] x3 = {-1, 4, 48, 12, 8};
         int[] x4 = {-1, -2, -3, -4, 21};
-
+        // display the HCF value of the each array.
         System.out.println(findHCF(5 , x));
         System.out.println(findHCF(5, x1));
         System.out.println(findHCF(3, x2));
@@ -18,6 +18,7 @@ public class Question2
     }
     public static int findHCF(int num, int[] arr)
     {
+        //store the positive value of array element into new arraylist
         ArrayList<Integer> posarr = new ArrayList<Integer>();
         for(int i = 0 ; i < arr.length; i++)
         {
@@ -26,14 +27,16 @@ public class Question2
                 posarr.add(arr[i]);
             }
         }
+        //run the recursion in order to the getting the hcf factor by repetition of modulus. 
         int temp = posarr.get(0);
         for(int i = 0 ; i < posarr.size(); i++)
         {
             temp = hcf(posarr.get(i), temp);
         }
-
+        //return the highest common factor of the each array
         return temp;
     }
+    //check array element by modulus based on previous element
     public static int hcf(int x , int y )
     {
         if (x <= 0)
